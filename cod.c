7710f8bc10f5a61c99facb3d75d3f21de64f5a74
murkyboo <anarbek8478@gmail.com>
2024-11-12 07:20:48 +0000

    gotovo

---
+++ cod.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#define TWO 2
+
+int main() {
+    int n;
+    std::cout << "Enter a natural number: ";
+    std::cin >> n;
+
+    int *result = new int(1);
+    int *i = new int(0);
+
+    while (*i < n){
+        *result *= TWO;
+        (*i)++;
+    }
+
+    std::cout << "2 to power of " << n << " is equal to " << *result << std::endl;
+
+    delete i;
+    delete result;
+
+    return 0;
+}
